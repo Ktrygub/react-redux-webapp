5cf537718dac12a1ed7cfe36a5c07f7eeabc44f0
Ktrygub <konstantin.trygub@gmail.com>
2017-12-26 02:24:45 +0200

    Auth process (signup/login/verify)

---
+++ src/components/messages/ConfirmEmailMessage.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import {Message} from 'semantic-ui-react'
+
+const ConfirmEmailMessage = () => <Message warning ><Message.Header>Please, verify your email</Message.Header></Message>
+
+export default ConfirmEmailMessage
